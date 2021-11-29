0bfc5ac9305f8c2c2e979a227774cbaa90464afa
ayeejane <cabanayan.jane@gmail.com>
2021-11-28 20:29:21 -0800

    Added styling

--- src/pages/createBlog.js
+++ src/pages/createBlog.js
@@ -5,69 +5,83 @@ import {
   Navbar
 } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { useHistory } from "react-router";
 import axios from "axios";
 
-export default function Blog() {
-  const [subject, setSubject] = useState("");
-  const [description, setDescription] = useState("");
-  const [tags, setTags] = useState("");
-
-  let history = useHistory();
-
-  const createPost = async (e) => {
-    e.preventDefault();
-    await axios.post('http://localhost:5000/postBlog', {
-      subject: subject,
-      description: description, 
-      tags: tags
-    }).then(()=>
-    { 
-      console.log('Success');
-      window.alert("New Post was created successfully.")
-      history.push("/home");
-    })
+class CreateBlog extends React.Component{
+  constructor(props)
+  {
+    super(props)
+    this.state={
+      subject: "",
+      description: "",
+      tags: ""
+    }
   }
 
-  return (
-    <div className="Blog">
-      <Navbar collapseOnSelect fixed='top' bg='dark'>
-        <h4 className="blogTitle">BLOG</h4>
-        <NavLink exact to = "/home" className="home">Back to Home</NavLink>
-      </Navbar>
-        <Form onSubmit={createPost}>
-          <Form.Group>
-            <Form.Label>Subject</Form.Label>
-            <Form.Control
-              autoFocus
-              placeholder="The future of blockchain"
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
-            >
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              autoFocus
-              placeholder="Blockchain is a buzz word nowadays..."
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            >
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Tags</Form.Label>
-            <Form.Control
-              autoFocus
-              placeholder="blockchain, bitcoin, decentralized"
-              value={tags}
-              onChange={(e) => setTags(e.target.value)}
-            >
-            </Form.Control>
-          </Form.Group>
-          <Button className="postBtn" type="submit">Add New Post</Button>
-        </Form>
-    </div>
-  );
+  createPost = async(e) => {
+    e.preventDefault();
+    try {
+      var subject = this.state.subject;
+      var description = this.state.description;
+      var tags = this.state.tags;
+      await axios.post('http://localhost:5000/postBlog', {
+        subject : subject,
+        description: description, 
+        tags: tags
+      })
+      .then((resp)=>  {
+        this.props.history.push("/home"); 
+      }, (error) => {
+        console.log(error);
+      });      
+    } catch (error) {
+      console.error("Error response:");
+      console.error(error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="Blog">
+        <Navbar collapseOnSelect fixed='top' bg='dark'>
+          <h4 className="blogTitle">BLOG</h4>
+          <NavLink exact to = "/home" className="home">Back to Home</NavLink>
+        </Navbar>
+          <Form onSubmit={this.createPost}>
+            <Form.Group>
+              <Form.Label>Subject</Form.Label>
+              <Form.Control
+                autoFocus
+                placeholder="The future of blockchain"
+                value={this.state.subject}
+                onChange={(e) => this.setSubject({subject: e.target.value})}
+              >
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                autoFocus
+                placeholder="Blockchain is a buzz word nowadays..."
+                value={this.state.description}
+                onChange={(e) => this.setDescription({description: e.target.value})}
+              >
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Tags</Form.Label>
+              <Form.Control
+                autoFocus
+                placeholder="blockchain, bitcoin, decentralized"
+                value={this.state.tags}
+                onChange={(e) => this.setTags({tags: e.target.value})}
+              >
+              </Form.Control>
+            </Form.Group>
+            <Button className="postBtn" type="submit">Add New Post</Button>
+          </Form>
+      </div>
+    );
+  }
 }
+export default CreateBlog;
